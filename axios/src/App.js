8319e92d90e351a0a75e72cadb1dfa3ca7adf5d7
DS <32024572+DineshSainath@users.noreply.github.com>
2024-06-14 13:35:18 +0000

    Apis.js for CRUD

--- axios/src/App.js
+++ axios/src/App.js
@@ -1,9 +1,36 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import {
+  fetchUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+} from "./components/Apis";
+import axios from "axios";
 
 function App() {
+  const url = "https://jsonplaceholder.typicode.com/users";
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => setData(response.data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  console.log(data);
+
   return (
     <div className="App">
       <h1>Axios</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>
+            <strong>{user.name}</strong>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
